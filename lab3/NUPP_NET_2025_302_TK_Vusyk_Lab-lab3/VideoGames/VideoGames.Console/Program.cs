c492ced6dc6714a126d7f8e69078741aa32326d2
KirillV2005 <128785969+KirillV2005@users.noreply.github.com>
2025-06-06 02:44:13 +0300

    Add files via upload

---
+++ lab3/NUPP_NET_2025_302_TK_Vusyk_Lab-lab3/VideoGames/VideoGames.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoGames.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using VideoGames.Infrastructure;
+using VideoGames.Infrastructure.Models;
+using VideoGames.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
+
+
+namespace VideoGames.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<VideoGamesContext>(options =>
+                options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=VideoGamesDB;Trusted_Connection=True;"));
+
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            builder.Services.AddScoped(typeof(ICrudServiceAsync<>), typeof(CrudServiceAsync<>));
+
+            var app = builder.Build();
+            app.Run();
+        }
+    }
+}
